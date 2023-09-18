06a80427f8e00312b5e1583454e9f0fdfd6e924a
grant <grant.lindsay@redspace.com>
2023-09-18 17:34:37 -0400

    show loading spinner until we have a post or not.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { PostsStore } from '../store/posts.store';
 import { UsersStore } from '../store/users.store';
 import { PostWithAuthor } from '../models/post-with-author';
@@ -13,7 +13,10 @@ import { PostWithAuthor } from '../models/post-with-author';
 })
 export class PostComponent implements OnInit {
 
+  private loadingSub = new BehaviorSubject<boolean>(true);
+
   data$: Observable<PostWithAuthor | null> | undefined;
+  loading$: Observable<boolean> = this.loadingSub.asObservable();
 
   constructor(
     private router: ActivatedRoute,
@@ -27,14 +30,18 @@ export class PostComponent implements OnInit {
 
     this.data$ = this.postsStore.getPostById(postId)
       .pipe(
+        tap(_ => this.loadingSub.next(false)),
         filter(post => !!post),
         switchMap(post => {
             return this.usersStore.getUserById(post.authorId)
               .pipe(
+                filter(user => !!user),
                 map(author => ({ post, author })),
               );
           }
         )
       );
+
+    this.data$.subscribe();
   }
 }
