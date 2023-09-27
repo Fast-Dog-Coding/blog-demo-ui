87410b5cc302e2d55be2064cc7bad52a6c376322
grant <grant.lindsay@redspace.com>
2023-09-27 17:07:07 -0400

    fixed issue: no update on param change.

--- src/app/posts-filtered/logic/posts-filtered.component.ts
+++ src/app/posts-filtered/logic/posts-filtered.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 import { PostWithAuthor } from '../../models/post-with-author';
 import { PostsStore } from '../../store/posts.store';
 import { UsersStore } from '../../store/users.store';
@@ -10,22 +10,24 @@ import { HttpRequestState } from '../../store/http-request-state';
 @Component({
   selector: 'app-posts-filtered',
   templateUrl: './posts-filtered.component.html',
-  styleUrls: ['./posts-filtered.component.css']
+  styleUrls: [ './posts-filtered.component.css' ]
 })
 export class PostsFilteredComponent implements OnInit {
 
   filteredPostsRequestData$: Observable<HttpRequestState<PostWithAuthor[]>> = of({ isLoading: true });
 
   constructor(
-    private router: ActivatedRoute,
+    private route: ActivatedRoute,
     private postsStore: PostsStore,
     private usersStore: UsersStore
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    const query = this.router.snapshot.queryParams;
-    this.filteredPostsRequestData$ = this.postsStore.loadFilteredPosts(query)
+    // Watch for changes in the query and then update the posts
+    this.filteredPostsRequestData$ = this.route.queryParams
       .pipe(
+        switchMap(query => this.postsStore.loadFilteredPosts(query)),
         processPosts(this.usersStore)
       );
   }
